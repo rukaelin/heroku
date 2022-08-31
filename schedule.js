0435c06491aa2a305c54ee57e7bf5a681db471dd
Ruben Kälin <ruben.kaelin@gmail.com>
2022-08-31 19:29:18 +0200

    fix date format

--- schedule.js
+++ schedule.js
@@ -2,9 +2,18 @@ module.exports = {
     setupSchedules: function () {
         const { sendMessage } = require('./telegram');
         const schedule = require('node-schedule');
-        sendMessage("messages scheduled")
         schedule.scheduleJob('*/10 * * * *', () => {
-            sendMessage("it is currently " + Date.now().toString())
+            sendMessage("it is currently " + (new Date()).toString())
         });
+    },
+    getCssImageUrl: function (document) {
+        const axios = require('axios');
+        const html = axios.get('https://enjoy365.ch/top-deals/');
+        const tempDocument = document.implentation.createHTMLDocument()
+        const tempHtml = document.createElement("html");
+        tempHtml.innerHTML = html;
+        var links = tempHtml.getElementsByClassName("product-image-link");
+        console.log(links[0]);
+        return links[0];
     }
 }
