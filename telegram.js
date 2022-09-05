56444f6d1268d97a8eb7fc9d865931f1d14b901e
Ruben Kälin <ruben.kaelin@gmail.com>
2022-09-05 20:47:50 +0200

    fix small bug

--- telegram.js
+++ telegram.js
@@ -3,22 +3,22 @@ module.exports = {
         const axios = require('axios');
         try {
             const truncatedMessage = message.substring(0, 200);
-            module.exports.doSendMessage(process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT_ID, truncatedMessage);
-            module.exports.doSendMessage(process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT_ID_SARAH, truncatedMessage);
+            module.exports.doSendMessage(process.env.TELEGRAM_CHAT_ID, truncatedMessage);
+            module.exports.doSendMessage(process.env.TELEGRAM_CHAT_ID_SARAH, truncatedMessage);
         } catch (err) {
             console.error(err);
         }
     },
-    doSendMessage: function (telegramToken, telegramChatId, message) {
+    doSendMessage: function (telegramChatId, message) {
         const axios = require('axios');
         axios
-            .post('https://api.telegram.org/bot' + telegramToken + '/sendMessage',
+            .post('https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/sendMessage',
                 {
                     chat_id: telegramChatId,
                     text: message
                 })
             .then(res => {
-                console.log(`message sent: ${truncatedMessage} to Ruben`);
+                console.log(`message sent: ${message} to Ruben`);
             })
             .catch(error => {
                 console.error(error);
